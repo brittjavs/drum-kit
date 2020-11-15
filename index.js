ade790e325ab0ee879fa5f4d8d328fae50e602ab
Brittany Javalera <brittjavalera@gmail.com>
2020-11-15 14:59:55 -0800

    added background image for each button and added a sound for each button through switch/case statement

---
+++ index.js
@@ -0,0 +1,38 @@
+for(let i=0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+}
+function handleClick(){
+   let buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML){
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3")
+                tom1.play()
+                break;
+            case "a":
+                let tom2 = new Audio("sounds/tom-2.mp3")
+                tom2.play()
+                break;
+            case "s":
+                let snare = new Audio("sounds/snare.mp3")
+                snare.play()
+                break;
+            case "d":
+                let tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play()
+                break;
+            case "j":
+                let tom4 = new Audio("sounds/tom-4.mp3")
+                tom4.play()
+                break;
+            case "k":
+                let kick = new Audio("sounds/kick-bass.mp3")
+                kick.play()
+                break;
+            case "l":
+                let crash = new Audio("sounds/crash.mp3")
+                crash.play()
+                break;
+            default:
+                console.log(this.innerHTML)
+        }
+}
