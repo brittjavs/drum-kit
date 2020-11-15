e126d3fe2ffa960b2f8528e5d6ad28f24491ee7d
Brittany Javalera <brittjavalera@gmail.com>
2020-11-15 15:12:07 -0800

    refactored code to accept key press and click events which both will call same makeSound function and pass different arguments

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 for(let i=0; i < document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        let buttonInnerHTML = this.innerHTML
+        makeSound(buttonInnerHTML)
+    })
 }
-function handleClick(){
-   let buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML){
-            case "w":
-                let tom1 = new Audio("sounds/tom-1.mp3")
-                tom1.play()
-                break;
-            case "a":
-                let tom2 = new Audio("sounds/tom-2.mp3")
-                tom2.play()
-                break;
-            case "s":
-                let snare = new Audio("sounds/snare.mp3")
-                snare.play()
-                break;
-            case "d":
-                let tom3 = new Audio("sounds/tom-3.mp3")
-                tom3.play()
-                break;
-            case "j":
-                let tom4 = new Audio("sounds/tom-4.mp3")
-                tom4.play()
-                break;
-            case "k":
-                let kick = new Audio("sounds/kick-bass.mp3")
-                kick.play()
-                break;
-            case "l":
-                let crash = new Audio("sounds/crash.mp3")
-                crash.play()
-                break;
-            default:
-                console.log(this.innerHTML)
-        }
-}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key)
+})
+
+function makeSound(key){
+         switch (key){
+             case "w":
+                 let tom1 = new Audio("sounds/tom-1.mp3")
+                 tom1.play()
+                 break;
+             case "a":
+                 let tom2 = new Audio("sounds/tom-2.mp3")
+                 tom2.play()
+                 break;
+             case "s":
+                 let snare = new Audio("sounds/snare.mp3")
+                 snare.play()
+                 break;
+             case "d":
+                 let tom3 = new Audio("sounds/tom-3.mp3")
+                 tom3.play()
+                 break;
+             case "j":
+                 let tom4 = new Audio("sounds/tom-4.mp3")
+                 tom4.play()
+                 break;
+             case "k":
+                 let kick = new Audio("sounds/kick-bass.mp3")
+                 kick.play()
+                 break;
+             case "l":
+                 let crash = new Audio("sounds/crash.mp3")
+                 crash.play()
+                 break;
+             default:
+                 console.log(this.innerHTML)
+         }
+ }
